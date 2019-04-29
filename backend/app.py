440139126e33777eccc9a6853e00428f250c6d41
suryakumarigoluguri <49465013+suryakumarigoluguri@users.noreply.github.com>
2019-04-29 18:58:13 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,4 @@ def index():
     return json.dumps(pk)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host='localhost', port=8006, debug=True)
-=======
-    app.run(host='localhost', port=8006, debug=True)
->>>>>>> dc2669fcc75bc383d2b9c897964d5e75de3e9bc0
